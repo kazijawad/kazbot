e1891e6a69b3fd8021a228d5353d3f4eb4b39064
Kazi Jawad <kazi.a.jawad@gmail.com>
2018-03-09 23:00:35 -0500

    Added urban dictionary cmd!

---
+++ commands/urban.js
@@ -0,0 +1,30 @@
+const discord = require('discord.js');
+const urban = require('relevant-urban');
+
+module.exports = {
+	name: 'urban',
+	aliases: ['ud'],
+	description: 'Search words from Urban Dictionary!',
+	args: true,
+	usage: '[WORD]',
+	cooldown: 5,
+	execute(message, args) {
+		const phrase = args.join(' ');
+
+		urban(phrase)
+			.then((term) => {
+				const phraseEmbed = new discord.RichEmbed()
+					.setColor('LIGHT_GREY')
+					.setTitle(term.word)
+					.addField('Definition', term.definition)
+					.addField('Example', term.example)
+					.addField('Author', term.author, true)
+					.addField('Thumbs Up', term.thumbsUp, true)
+					.addField('Thumbs Down', term.thumbsDown, true)
+					.setFooter('@Kaz-Bot')
+					.setTimestamp(new Date());
+
+				message.channel.send({ embed: phraseEmbed });
+			});
+	},
+};
