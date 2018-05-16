065dcecedc76f7319cf20b77b69fcbfb0180c6cf
Kazi Jawad <kazi.a.jawad@gmail.com>
2018-05-16 18:58:10 -0400

    New song command

---
+++ commands/music/song.js
@@ -0,0 +1,26 @@
+const { Command } = require('discord.js-commando');
+
+module.exports = class SongCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'song',
+			group: 'music',
+			memberName: 'song',
+			description: 'Displays the song currently playing.',
+			examples: ['song'],
+			guildOnly: true,
+		});
+	}
+
+	async run(message) {
+		const queue = this.queue.get(message.guild.id);
+		if (!queue) return message.reply('There is no music playing right now.');
+
+		message.say(`Now Playing: ${queue.songs[0].title}`);
+	}
+
+	get queue() {
+		if (!this._queue) this._queue = this.client.registry.resolveCommand('music:play').queue;
+		return this._queue;
+	}
+};
