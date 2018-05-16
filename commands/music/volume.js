fc39d3e600b07e83c83a38c41324583cb398dbd3
Kazi Jawad <kazi.a.jawad@gmail.com>
2018-05-16 19:20:56 -0400

    New volume command to display volume and control volume

---
+++ commands/music/volume.js
@@ -0,0 +1,44 @@
+const { Command } = require('discord.js-commando');
+
+module.exports = class VolumeCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'volume',
+			aliases: ['vol'],
+			group: 'music',
+			memberName: 'volume',
+			description: 'Sets the current volume for Kaz Bot.',
+			examples: ['volume'],
+			guildOnly: true,
+			args: [
+				{
+					key: 'level',
+					prompt: 'What would you like to set the volume at?',
+					type: 'integer',
+					default: '',
+				},
+			],
+		});
+	}
+
+	async run(message, { level }) {
+		const queue = this.queue.get(message.guild.id);
+		const voiceChannel = message.member.voiceChannel;
+
+		if (!queue) return message.reply('There is no music playing right now.');
+		if (!voiceChannel) return message.say('Please join a voice channel first!');
+
+		if (level === '') {
+			return message.say(`Current Volume: ${queue.volume}`);
+		} else {
+			queue.volume = level;
+			queue.connection.dispatcher.setVolumeLogarithmic(level / 5);
+			message.say(`Current Volume: ${queue.volume}`);
+		}
+	}
+
+	get queue() {
+		if (!this._queue) this._queue = this.client.registry.resolveCommand('music:play').queue;
+		return this._queue;
+	}
+};
