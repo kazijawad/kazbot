85c3bc431a4f97dedafa8101e5a314aa501db89e
Kazi Jawad <kazi.a.jawad@gmail.com>
2018-08-26 22:44:20 -0400

    Update credit command with new embed format

--- commands/utilities/credit.js
+++ commands/utilities/credit.js
@@ -1,33 +1,73 @@
 const { Command } = require('discord.js-commando');
-const { RichEmbed } = require('discord.js');
 
-module.exports = class CreditCommand extends Command {
+class CreditCommand extends Command {
 	constructor(client) {
 		super(client, {
 			name: 'credit',
+			aliases: ['source'],
 			group: 'misc',
 			memberName: 'credit',
-			description: 'Credit to all the Bot\'s Resources.',
+			description: 'Credit to KazBot\'s Resources.',
 			examples: ['credit'],
 			guildOnly: false,
 		});
 	}
 
 	async run(message) {
-		const creditEmbed = new RichEmbed()
-			.setColor('GOLD')
-			.setTitle('Credits')
-			.addField('Discord.js', 'https://discord.js.org/#/')
-			.addField('Fortnite API Wrapper', 'https://github.com/qlaffont/fortnite-api')
-			.addField('Battlerite API', 'https://developer.battlerite.com')
-			.addField('Overwatch API Wrapper', 'https://github.com/gclem/overwatch-js')
-			.addField('League of Legends API', 'https://developer.riotgames.com/api-methods/')
-			.addField('CoinMarketCap API', 'https://coinmarketcap.com/api/')
-			.addField('Youtube Download Module', 'https://github.com/fent/node-ytdl-core')
-			.addField('Github Repository', 'https://github.com/kazijawad/kaz-bot')
-			.setFooter('@Kaz-Bot')
-			.setTimestamp(new Date());
+		const creditEmbed = {
+			color: 0xffd700,
+			title: 'Credit',
+			author: {
+				name: 'YellowJay',
+				icon_url: process.env.AVATAR_URL,
+				url: 'https://kazijawad.github.io/',
+			},
+			thumbnail: {
+				url: message.client.user.avatarURL,
+			},
+			fields: [
+				{
+					name: 'Discord.js',
+					value: 'https://discord.js.org/#/',
+				},
+				{
+					name: 'Fortnite API Wrapper',
+					value: 'https://github.com/qlaffont/fortnite-api',
+				},
+				{
+					name: 'Battlerite API',
+					value: 'https://developer.battlerite.com',
+				},
+				{
+					name: 'Overwatch API Wrapper',
+					value: 'https://github.com/gclem/overwatch-js',
+				},
+				{
+					name: 'League of Legends API',
+					value: 'https://developer.riotgames.com/api-methods',
+				},
+				{
+					name: 'CoinMarketCap API',
+					value: 'https://coinmarketcap.com/api',
+				},
+				{
+					name: 'Youtube Download Module',
+					value: 'https://github.com/fent/node-ytdl-core',
+				},
+				{
+					name: 'Github Repository',
+					value: 'https://github.com/kazijawad/KazBot',
+				},
+			],
+			timestamp: new Date(),
+			footer: {
+				text: '@KazBot',
+				icon_url: message.client.user.avatarURL,
+			},
+		};
 
 		message.embed(creditEmbed);
 	}
-};
+}
+
+module.exports = CreditCommand;
