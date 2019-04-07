8a03e6de520f900ee6c70c79d705442fe1921e29
Kazi Jawad <kazi.a.jawad@gmail.com>
2019-04-07 15:32:42 -0400

    Change to async/await

--- src/commands/crypto/crypto.js
+++ src/commands/crypto/crypto.js
@@ -37,129 +37,127 @@ class CryptoCommand extends Command {
 
 	async run(message, { coin, market }) {
 		if (coin === 'global') {
-			instance.get('/v1/global-metrics/quotes/latest', { params: { convert: market } })
-				.then(response => {
-					const global = response['data']['data'];
-					const cryptoGlobal = {
-						color: 0x008000,
-						title: 'Global Crypto Info',
-						author: {
-							name: 'KazBot',
-							icon_url: process.env.AVATAR_URL,
-							url: 'https://kazijawad.github.io/',
-						},
-						fields: [
-							{
-								name: 'Bitcoin Dominance',
-								value: global['btc_dominance'].toString(),
-								inline: true,
-							},
-							{
-								name: 'Ethereum Dominance',
-								value: global['eth_dominance'].toString(),
-								inline: true,
-							},
-							{
-								name: 'Active Cryptocurrencies',
-								value: global['active_cryptocurrencies'],
-								inline: true,
-							},
-							{
-								name: 'Active Market Pairs',
-								value: global['active_market_pairs'],
-								inline: true,
-							},
-							{
-								name: 'Active Exchanges',
-								value: global['active_exchanges'],
-								inline: true,
-							},
-							{
-								name: 'Total Market Cap',
-								value: global['quote'][market]['total_market_cap'].toString(),
-								inline: true,
-							},
-							{
-								name: 'Total 24hr Volume',
-								value: global['quote'][market]['total_volume_24h'].toString(),
-								inline: true,
-							},
-						],
-						timestamp: new Date(),
-						footer: {
-							text: '@KazBot',
-							icon_url: message.client.user.avatarURL,
-						},
-					};
+			try {
+				const response = await instance.get('/v1/global-metrics/quotes/latest', { params: { convert: market } });
+				const global = response['data']['data'];
 
-					message.embed(cryptoGlobal);
-				})
-				.catch(error => {
-					console.warn(`COIN API: ${error}`);
-					message.say('Failed to locate cryptocurrency in CoinMarketCap!');
-				});
+				const cryptoGlobal = {
+					color: 0x008000,
+					title: 'Global Crypto Info',
+					author: {
+						name: 'KazBot',
+						icon_url: process.env.AVATAR_URL,
+						url: 'https://kazijawad.github.io/',
+					},
+					fields: [
+						{
+							name: 'Bitcoin Dominance',
+							value: global['btc_dominance'].toString(),
+							inline: true,
+						},
+						{
+							name: 'Ethereum Dominance',
+							value: global['eth_dominance'].toString(),
+							inline: true,
+						},
+						{
+							name: 'Active Cryptocurrencies',
+							value: global['active_cryptocurrencies'],
+							inline: true,
+						},
+						{
+							name: 'Active Market Pairs',
+							value: global['active_market_pairs'],
+							inline: true,
+						},
+						{
+							name: 'Active Exchanges',
+							value: global['active_exchanges'],
+							inline: true,
+						},
+						{
+							name: 'Total Market Cap',
+							value: global['quote'][market]['total_market_cap'].toString(),
+							inline: true,
+						},
+						{
+							name: 'Total 24hr Volume',
+							value: global['quote'][market]['total_volume_24h'].toString(),
+							inline: true,
+						},
+					],
+					timestamp: new Date(),
+					footer: {
+						text: '@KazBot',
+						icon_url: message.client.user.avatarURL,
+					},
+				};
+				message.embed(cryptoGlobal);
+			} catch (error) {
+				console.warn(`COIN API: ${error.message}`);
+				message.say('Failed to locate cryptocurrency in CoinMarketCap.');
+			}
 		} else {
-			instance.get('/v1/cryptocurrency/quotes/latest', { params: { symbol: coin.toUpperCase(), convert: market } })
-				.then(response => {
-					const crypto = response['data']['data'][coin.toUpperCase()];
-					const cryptoInfo = {
-						color: 0x008000,
-						title: crypto['name'],
-						author: {
-							name: 'KazBot',
-							icon_url: process.env.AVATAR_URL,
-							url: 'https://kazijawad.github.io/',
-						},
-						fields: [
-							{
-								name: 'Symbol',
-								value: crypto['symbol'],
-								inline: true,
-							},
-							{
-								name: 'Rank',
-								value: crypto['cmc_rank'],
-								inline: true,
-							},
-							{
-								name: 'Price',
-								value: crypto['quote'][market]['price'].toString(),
-								inline: true,
-							},
-							{
-								name: 'Market Cap',
-								value: crypto['quote'][market]['market_cap'].toString(),
-								inline: true,
-							},
-							{
-								name: 'Circulating Supply',
-								value: crypto['circulating_supply'],
-								inline: true,
-							},
-							{
-								name: 'Total Supply',
-								value: crypto['total_supply'],
-								inline: true,
-							},
-							{
-								name: 'Max Supply',
-								value: crypto['max_supply'],
-								inline: true,
-							},
-						],
-						timestamp: new Date(),
-						footer: {
-							text: '@KazBot',
-							icon_url: message.client.user.avatarURL,
-						},
-					};
+			try {
+				const response = await instance.get('/v1/cryptocurrency/quotes/latest', { params: { symbol: coin.toUpperCase(), convert: market } });
+				const crypto = response['data']['data'][coin.toUpperCase()];
 
-					message.embed(cryptoInfo);
-				})
-				.catch(error => {
-					console.warn(`COIN API: ${error}`);
-					message.say('Failed to retrieve global information from CoinMarketCap!');
-				});
+				const cryptoInfo = {
+					color: 0x008000,
+					title: crypto['name'],
+					author: {
+						name: 'KazBot',
+						icon_url: process.env.AVATAR_URL,
+						url: 'https://kazijawad.github.io/',
+					},
+					fields: [
+						{
+							name: 'Symbol',
+							value: crypto['symbol'],
+							inline: true,
+						},
+						{
+							name: 'Rank',
+							value: crypto['cmc_rank'],
+							inline: true,
+						},
+						{
+							name: 'Price',
+							value: crypto['quote'][market]['price'].toString(),
+							inline: true,
+						},
+						{
+							name: 'Market Cap',
+							value: crypto['quote'][market]['market_cap'].toString(),
+							inline: true,
+						},
+						{
+							name: 'Circulating Supply',
+							value: crypto['circulating_supply'],
+							inline: true,
+						},
+						{
+							name: 'Total Supply',
+							value: crypto['total_supply'],
+							inline: true,
+						},
+						{
+							name: 'Max Supply',
+							value: crypto['max_supply'],
+							inline: true,
+						},
+					],
+					timestamp: new Date(),
+					footer: {
+						text: '@KazBot',
+						icon_url: message.client.user.avatarURL,
+					},
+				};
+				message.embed(cryptoInfo);
+			} catch (error) {
+				console.warn(`COIN API: ${error.message}`);
+				message.say('Failed to retrieve global information from CoinMarketCap.');
+			}
 		}
 	}
 }
