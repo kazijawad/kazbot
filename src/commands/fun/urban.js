eeaeeaf0ca6f7129daff08e6e32f10dec81a928f
Kazi Jawad <kazi.a.jawad@gmail.com>
2019-04-07 15:26:16 -0400

    Switch to async/await

--- src/commands/fun/urban.js
+++ src/commands/fun/urban.js
@@ -26,59 +26,58 @@ class UrbanCommand extends Command {
 	}
 
 	async run(message, { phrase }) {
-		instance.get('/define', { params: { term: phrase } })
-			.then(response => {
-				const term = response['data']['list'][0];
-				if (!term) {
-					return message.say(`${phrase} cannot be found on Urban Dictionary`);
-				}
-				const termExample = term['example'] ? term['example'] : 'N/A';
+		try {
+			const response = await instance.get('/define', { params: { term: phrase } });
+			const term = response['data']['list'][0];
+			if (!term) {
+				return message.say(`${phrase} cannot be found on Urban Dictionary.`);
+			}
+			const termExample = term['example'] ? term['example'] : 'N/A';
 
-				const termEmbed = {
-					color: 0xd3d3d3,
-					title: term['word'],
-					author: {
-						name: 'KazBot',
-						icon_url: process.env.AVATAR_URL,
-						url: 'https://kazijawad.github.io/',
+			const termEmbed = {
+				color: 0xd3d3d3,
+				title: term['word'],
+				author: {
+					name: 'KazBot',
+					icon_url: process.env.AVATAR_URL,
+					url: 'https://kazijawad.github.io/',
+				},
+				fields: [
+					{
+						name: 'Definition',
+						value: term['definition'],
 					},
-					fields: [
-						{
-							name: 'Definition',
-							value: term['definition'],
-						},
-						{
-							name: 'Example',
-							value: termExample,
-						},
-						{
-							name: 'Author',
-							value: term['author'],
-							inline: true,
-						},
-						{
-							name: 'Thumbs Up',
-							value: term['thumbs_up'],
-							inline: true,
-						},
-						{
-							name: 'Thumbs Down',
-							value: term['thumbs_down'],
-							inline: true,
-						},
-					],
-					timestamp: new Date(),
-					footer: {
-						text: '@KazBot',
-						icon_url: message.client.user.avatarURL,
+					{
+						name: 'Example',
+						value: termExample,
 					},
-				};
+					{
+						name: 'Author',
+						value: term['author'],
+						inline: true,
+					},
+					{
+						name: 'Thumbs Up',
+						value: term['thumbs_up'],
+						inline: true,
+					},
+					{
+						name: 'Thumbs Down',
+						value: term['thumbs_down'],
+						inline: true,
+					},
+				],
+				timestamp: new Date(),
+				footer: {
+					text: '@KazBot',
+					icon_url: message.client.user.avatarURL,
+				},
+			};
 
-				message.embed(termEmbed);
-			})
-			.catch(() => {
-				message.say('Failed to retrieve word from Urban Dictionary.');
-			});
+			message.embed(termEmbed);
+		} catch (error) {
+			message.say('Failed to retrieve word from Urban Dictionary.');
+		}
 	}
 }
 
