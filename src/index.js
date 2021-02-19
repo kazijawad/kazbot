11b5b6ccc500e7036415bdd5ce28b1fc58010ba7
Kazi Jawad <kazi.a.jawad@gmail.com>
2021-02-18 22:08:20 -0500

    remove DBL call

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ require('dotenv').config();
 
 const path = require('path');
 const { CommandoClient } = require('discord.js-commando');
-const axios = require('axios');
 
 const client = new CommandoClient({
 	owner: '221449635254894594',
@@ -11,25 +10,6 @@ const client = new CommandoClient({
 	disableEveryone: true,
 	unknownCommandResponse: false,
 });
-
-const instance = axios.create({
-	baseURL: 'https://discord.bots.gg/api/v1',
-	headers: {
-		'Authorization': process.env.DBL_API,
-		'Content-Type': 'application/json',
-	},
-});
-
-async function recordServerCount() {
-	try {
-		console.info(`[SERVER COUNT] ${client.guilds.size}`);
-		await instance.post(`/bots/${process.env.CLIENT_ID}/stats`, {
-			guildCount: client.guilds.size,
-		});
-	} catch (error) {
-		console.error(`[DBL ERROR] ${error}`);
-	}
-}
 
 client.registry
 	.registerDefaults()
@@ -44,9 +24,9 @@ client.registry
 client.on('ready', () => {
 	console.info(`[ONLINE] ${client.user.tag}!`);
 	if (process.env.NODE_ENV === 'production') {
-		recordServerCount();
+		console.info(`[SERVER COUNT] ${client.guilds.size}`);
 		setInterval(() => {
-			recordServerCount();
+			console.info(`[SERVER COUNT] ${client.guilds.size}`);
 		}, 1000 * 60 * 60);
 	}
 	client.user.setPresence({ game: { name: 'k!help' } });
